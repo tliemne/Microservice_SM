cfe5ab1dd839bbd6c6846b358dbc92f2e15fdfbe
Nam Nguyen <vannam2612@gmail.com>
2026-02-02 22:29:00 +0700

    feat(api-gateway): config routing

--- api-gateway/src/main/java/com/microservices/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/microservices/api_gateway/ApiGatewayApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.microservices.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
